1d5a1d22515275824e095b20dc580ada1bff6710
Rüçhan Adıgüzel <44341663+ruchan-o7@users.noreply.github.com>
2024-12-12 13:10:21 +0300

    remap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -22,7 +22,7 @@ return {
 		local builtin = require 'telescope.builtin'
 		vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Telescope find files' })
 		vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Telescope live grep' })
-		vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = 'Telescope buffers' })
+		vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'Telescope buffers' })
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 	end,
 }
